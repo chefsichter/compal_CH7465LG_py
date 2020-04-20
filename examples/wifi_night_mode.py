9c964c23cc2197f974d067407851b50927efe220
chefsichter <51502336+chefsichter@users.noreply.github.com>
2020-04-20 13:04:06 +0200

    wifi night mode

--- examples/wifi_night_mode.py
+++ examples/wifi_night_mode.py
@@ -8,10 +8,13 @@ from compal import Compal, WifiSettings
 modem = Compal(os.environ['router_IP'], os.environ['router_ACCESS'])
 modem.login()
 
+print("Succesful logged into router")
 # restore_wifi_settings(modem)
 wifi = WifiSettings(modem)
 wifi.turn_off()
+print("Turned off both wifi")
 
 # Wait for 8h
 time.sleep(5 * 60)
 wifi.turn_on_2g()
+print("Turned on 2g")
